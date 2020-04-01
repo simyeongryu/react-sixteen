f652767d2d05bd6525e39d8912379f19fa561bcc
simyeongryu <rsm0503@gmail.com>
2020-04-01 15:22:43 +0900

    #6

--- README.md
+++ README.md
@@ -425,3 +425,60 @@ class App extends Component {
 // export할 때도 보호할 수 있다.
 export default BoundaryHOC(App);
 ```
+
+# #6 this.setState(null)
+
+컴포넌트 업데이트 프로세스
+
+setState를 하면 컴포넌트는 반드시 업데이트 된다.
+
+근데 이걸 제어할 수 있다. 
+
+언제 컴포넌트를 업데이트할지 결정할 수 있게 한다. 
+
+setState(null)을 해주면, state와 컴포넌트의 업데이트가 멈춘다.
+
+```js
+import React, { Component, Fragment } from "react";
+
+const MAX_PIZZAS = 20;
+
+// setState 형식에 맞춤
+const eatPizza = (state, props) => {
+  const { pizzas } = state;
+  if (pizzas < MAX_PIZZAS) {
+    return {
+      pizzas: pizzas + 1
+    };
+  } else {
+    // null이 되면 state도 안 바뀌고, 컴포넌트도 변하지 않는다.
+    return null;
+  }
+};
+
+class Contorlled extends Component {
+  state = {
+    pizzas: 0
+  };
+  render() {
+    const { pizzas } = this.state;
+    return (
+      <button onClick={this._handleClick}>{`I have eaten ${pizzas} ${
+        pizzas === 1 ? "pizza" : "pizzas"
+      } `}</button>
+    );
+  }
+  // component 밖에서 setState를 사용할 수 있는 방법
+  _handleClick = () => {
+    this.setState(eatPizza);
+  };
+}
+class App extends Component {
+  render = () => {
+    return <Contorlled />;
+  };
+}
+
+// export할 때도 보호할 수 있다.
+export default App;
+```
--- src/App.js
+++ src/App.js
@@ -1,94 +1,42 @@
 import React, { Component, Fragment } from "react";
-import { createPortal } from "react-dom";
 
-// HOC
-const BoundaryHOC = ProtectedComponent =>
-  class Boundary extends Component {
-    state = {
-      hasError: false
+const MAX_PIZZAS = 20;
+
+// setState 형식에 맞춤
+const eatPizza = (state, props) => {
+  const { pizzas } = state;
+  if (pizzas < MAX_PIZZAS) {
+    return {
+      pizzas: pizzas + 1
     };
-
-    // error가 발생하면
-    componentDidCatch = (error, info) => {
-      console.log(`ERROR: ${error} // INFO: ${JSON.stringify(info)}`);
-      this.setState({
-        hasError: true
-      });
-    };
-
-    render() {
-      const { hasError } = this.state;
-      // error가 발생하면 FallbackUI 아니면 파라미터로 받은 component
-      if (hasError) {
-        return <ErrorFallback />;
-      } else {
-        return <ProtectedComponent />;
-      }
-    }
-  };
-
-class ErrorMaker extends Component {
-  state = {
-    friends: ["noel", "liam", "bonehead", "tony", "guigsy"]
-  };
-
-  // 2초 후에 state의 friends가 undefined로 변하며 Error
-  componentDidMount = () => {
-    setTimeout(() => {
-      this.setState({
-        friends: undefined
-      });
-    }, 2000);
-  };
-
-  render() {
-    const { friends } = this.state;
-    return friends.map(friend => ` ${friend} `);
+  } else {
+    // null이 되면 state도 안 바뀌고, 컴포넌트도 변하지 않는다.
+    return null;
   }
-}
+};
 
-// HOC로 보호
-const PErrorMaker = BoundaryHOC(ErrorMaker);
-
-// root 밖의 요소를 제어할 때 사용
-class Portals extends Component {
-  // 엘리먼트 대신 포탈을 return
-  // react 가 아니라 react-dom
-  render = () => {
-    // createPortal(component, 제어할 DOM)
-    return createPortal(<Message />, document.getElementById("touch"));
+class Contorlled extends Component {
+  state = {
+    pizzas: 0
+  };
+  render() {
+    const { pizzas } = this.state;
+    return (
+      <button onClick={this._handleClick}>{`I have eaten ${pizzas} ${
+        pizzas === 1 ? "pizza" : "pizzas"
+      } `}</button>
+    );
+  }
+  // component 밖에서 setState를 사용할 수 있는 방법
+  _handleClick = () => {
+    this.setState(eatPizza);
   };
 }
-// HOC로 보호
-const PPortals = BoundaryHOC(Portals);
-
-const Message = () => "Touch It";
-
-// 하나가 아니라 두 개 이상을 return 하고 싶다면 Fragment를 사용한다.
-// String도 return 할 수 있다.
-class ReturnTypes extends Component {
-  render = () => {
-    return "Hello WOW";
-  };
-}
-// HOC로 보호
-const PReturnTypes = BoundaryHOC(ReturnTypes);
-
-// 에러가 발생하면 대신 마운트되는 컴포넌트
-const ErrorFallback = () => "Sorry Something went wrong";
-
 class App extends Component {
   render = () => {
-    return (
-      <Fragment>
-        {/* HOC로 보호된 컴포넌트 */}
-        <PReturnTypes />
-        <PPortals />
-        <PErrorMaker />
-      </Fragment>
-    );
+    return <Contorlled />;
   };
 }
 
 // export할 때도 보호할 수 있다.
-export default BoundaryHOC(App);
+export default App;
